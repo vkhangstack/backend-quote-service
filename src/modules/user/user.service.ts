edf2de591eab757c39d7d7de6cba25f750f9cdc4
Pham Van Khang <phamvankhang.tvi@gmail.com>
2023-01-09 04:27:23 +0700

    write log

--- src/modules/user/user.service.ts
+++ src/modules/user/user.service.ts
@@ -1,23 +1,24 @@
-import { Injectable } from "@nestjs/common";
-import { CommandBus } from "@nestjs/cqrs";
-import { plainToClass } from "class-transformer";
-import type { FindConditions } from "typeorm";
-import { Transactional } from "typeorm-transactional-cls-hooked";
+import { Inject, Injectable } from '@nestjs/common';
+import { CommandBus } from '@nestjs/cqrs';
+import { plainToClass } from 'class-transformer';
+import type { FindConditions } from 'typeorm';
+import { Transactional } from 'typeorm-transactional-cls-hooked';
+import { Logger } from 'winston';
 
-import type { PageDto } from "../../common/dto/page.dto";
-import { FileNotImageException, UserNotFoundException } from "../../exceptions";
-import { IFile } from "../../interfaces";
+import type { PageDto } from '../../common/dto/page.dto';
+import { FileNotImageException, UserNotFoundException } from '../../exceptions';
+import { IFile } from '../../interfaces';
 // import { AwsS3Service } from '../../shared/services/aws-s3.service';
-import { ValidatorService } from "../../shared/services/validator.service";
-import type { Optional } from "../../types";
-import { UserRegisterDto } from "../auth/dto/UserRegisterDto";
-import { CreateSettingsCommand } from "./commands/create-settings.command";
-import { CreateSettingsDto } from "./dtos/create-settings.dto";
-import type { UserDto } from "./dtos/user.dto";
-import type { UsersPageOptionsDto } from "./dtos/users-page-options.dto";
-import type { UserEntity } from "./user.entity";
-import { UserRepository } from "./user.repository";
-import type { UserSettingsEntity } from "./user-settings.entity";
+import { ValidatorService } from '../../shared/services/validator.service';
+import type { Optional } from '../../types';
+import { UserRegisterDto } from '../auth/dto/UserRegisterDto';
+import { CreateSettingsCommand } from './commands/create-settings.command';
+import { CreateSettingsDto } from './dtos/create-settings.dto';
+import type { UserDto } from './dtos/user.dto';
+import type { UsersPageOptionsDto } from './dtos/users-page-options.dto';
+import type { UserEntity } from './user.entity';
+import { UserRepository } from './user.repository';
+import type { UserSettingsEntity } from './user-settings.entity';
 
 @Injectable()
 export class UserService {
@@ -25,7 +26,9 @@ export class UserService {
     private userRepository: UserRepository,
     private validatorService: ValidatorService,
     // private awsS3Service: AwsS3Service,
-    private commandBus: CommandBus
+    private commandBus: CommandBus,
+    @Inject('winston')
+    private readonly logger: Logger,
   ) {}
 
   /**
@@ -36,20 +39,20 @@ export class UserService {
   }
 
   async findByUsernameOrEmail(
-    options: Partial<{ username: string; email: string }>
+    options: Partial<{ username: string; email: string }>,
   ): Promise<Optional<UserEntity>> {
     const queryBuilder = this.userRepository
-      .createQueryBuilder("user")
-      .leftJoinAndSelect<UserEntity, "user">("user.settings", "settings");
+      .createQueryBuilder('user')
+      .leftJoinAndSelect<UserEntity, 'user'>('user.settings', 'settings');
 
     if (options.email) {
-      queryBuilder.orWhere("user.email = :email", {
+      queryBuilder.orWhere('user.email = :email', {
         email: options.email,
       });
     }
 
     if (options.username) {
-      queryBuilder.orWhere("user.username = :username", {
+      queryBuilder.orWhere('user.username = :username', {
         username: options.username,
       });
     }
@@ -60,13 +63,14 @@ export class UserService {
   @Transactional()
   async createUser(
     userRegisterDto: UserRegisterDto,
-    file: IFile
+    file: IFile,
   ): Promise<UserEntity> {
+    this.logger.info('Create User');
     const user = this.userRepository.create(userRegisterDto);
 
-    // if (file && !this.validatorService.isImage(file.mimetype)) {
-    //   throw new FileNotImageException();
-    // }
+    if (file && !this.validatorService.isImage(file.mimetype)) {
+      throw new FileNotImageException();
+    }
 
     // if (file) {
     //   user.avatar = await this.awsS3Service.uploadImage(file);
@@ -79,25 +83,25 @@ export class UserService {
       plainToClass(CreateSettingsDto, {
         isEmailVerified: false,
         isPhoneVerified: false,
-      })
+      }),
     );
 
     return user;
   }
 
   async getUsers(
-    pageOptionsDto: UsersPageOptionsDto
+    pageOptionsDto: UsersPageOptionsDto,
   ): Promise<PageDto<UserDto>> {
-    const queryBuilder = this.userRepository.createQueryBuilder("user");
+    const queryBuilder = this.userRepository.createQueryBuilder('user');
     const [items, pageMetaDto] = await queryBuilder.paginate(pageOptionsDto);
 
     return items.toPageDto(pageMetaDto);
   }
 
   async getUser(userId: Uuid): Promise<UserDto> {
-    const queryBuilder = this.userRepository.createQueryBuilder("user");
+    const queryBuilder = this.userRepository.createQueryBuilder('user');
 
-    queryBuilder.where("user.id = :userId", { userId });
+    queryBuilder.where('user.id = :userId', { userId });
 
     const userEntity = await queryBuilder.getOne();
 
@@ -110,10 +114,10 @@ export class UserService {
 
   async createSettings(
     userId: Uuid,
-    createSettingsDto: CreateSettingsDto
+    createSettingsDto: CreateSettingsDto,
   ): Promise<UserSettingsEntity> {
     return this.commandBus.execute<CreateSettingsCommand, UserSettingsEntity>(
-      new CreateSettingsCommand(userId, createSettingsDto)
+      new CreateSettingsCommand(userId, createSettingsDto),
     );
   }
 }
