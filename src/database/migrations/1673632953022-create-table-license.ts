824fbedd8885b096a43a1b7bf0c0d4cfc005e64b
Pham Van Khang <phamvankhang.tvi@gmail.com>
2023-01-14 14:55:19 +0700

    create  license  migratrion

---
+++ src/database/migrations/1673632953022-create-table-license.ts
@@ -0,0 +1,27 @@
+import type { MigrationInterface, QueryRunner } from 'typeorm';
+
+export class CreateTableLicense1673632953022 implements MigrationInterface {
+  name = 'createLicenseTable1622299662707';
+
+  public async up(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query(`
+        CREATE TABLE "licenses"
+        (
+          "id"         uuid  NOT NULL DEFAULT uuid_generate_v4(),
+          "created_at" TIMESTAMP NOT NULL DEFAULT now(),
+          "updated_at" TIMESTAMP,
+          "created_by" character varying,
+          "updated_by" character varying,
+          "user_id"    character varying,
+          "expires"    numeric,
+          "day_expire" numeric,
+          "type_license" numeric,
+          "status" numeric,
+          CONSTRAINT "PK_a3f2b1ccc241659fc62907c7433" PRIMARY KEY ("id")
+        )`);
+  }
+
+  public async down(queryRunner: QueryRunner): Promise<void> {
+    await queryRunner.query('DROP TABLE "licenses"');
+  }
+}
